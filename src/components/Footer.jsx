17a95fc9612fcc56abce624224a855ff3647c1d7
Shiri Zadok <shirizadok1@gmail.com>
2022-08-03 11:00:03 +0300

    last version

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,44 +1,54 @@
-import React from 'react';
+import React from "react"
 import {
   MDBFooter,
   MDBContainer,
   MDBCol,
   MDBRow,
-  MDBIcon
-} from 'mdb-react-ui-kit';
-
-
+  MDBIcon,
+} from "mdb-react-ui-kit"
 
 function Footer() {
-  const today = new Date();
-  const year = today.getFullYear();
+  const today = new Date()
+  const year = today.getFullYear()
 
   return (
-    <footer className='footer'>
-    <MDBFooter className='bg-dark text-center text-white'>
-      <MDBContainer className='p-4 pb-0'>
-        <section className='mb-4'>
-          <a className='btn btn-outline-light btn-floating m-1' href='https://www.facebook.com/shiri.zadok' role='button'>
-            <MDBIcon fab icon='facebook-f' />
-          </a>
-        
-          <a className='btn btn-outline-light btn-floating m-1' href='https://www.linkedin.com/in/shiri-zadok-868096116/' role='button'>
-            <MDBIcon fab icon='linkedin-in' />
+    <MDBFooter className="bg-dark text-center text-white fixed-bottom">
+      <MDBContainer className="p-4 pb-0">
+        <section className="mb-4">
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="https://www.facebook.com/shiri.zadok"
+            role="button"
+          >
+            <MDBIcon fab icon="facebook-f" />
           </a>
 
-          <a className='btn btn-outline-light btn-floating m-1' href='https://github.com/shirizadok1' role='button'>
-            <MDBIcon fab icon='github' />
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="https://www.linkedin.com/in/shiri-zadok-868096116/"
+            role="button"
+          >
+            <MDBIcon fab icon="linkedin-in" />
+          </a>
+
+          <a
+            className="btn btn-outline-light btn-floating m-1"
+            href="https://github.com/shirizadok1"
+            role="button"
+          >
+            <MDBIcon fab icon="github" />
           </a>
         </section>
       </MDBContainer>
-      
-      <div className='text-center p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
         © {year} Copyright: Shiri Zadok
       </div>
     </MDBFooter>
-    </footer>
-  );
+  )
 }
-    
 
-export default Footer;
+export default Footer
--- src/components/SearchArea.jsx
+++ src/components/SearchArea.jsx
@@ -1,25 +1,25 @@
-import React, { useState } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import InputGroup from "react-bootstrap/InputGroup";
-import FormControl from "react-bootstrap/FormControl";
-import Button from "react-bootstrap/Button";
-import SearchIcon from "@material-ui/icons/Search";
-import axios from "axios";
-import InfiniteScroll from 'react-infinite-scroll-component';
-import Card from "./Card";
-import { Container, Row, Col } from 'react-bootstrap';
-import "./Box.css";
+import React, { useState } from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import InputGroup from "react-bootstrap/InputGroup"
+import FormControl from "react-bootstrap/FormControl"
+import Button from "react-bootstrap/Button"
+import SearchIcon from "@material-ui/icons/Search"
+import axios from "axios"
+import InfiniteScroll from "react-infinite-scroll-component"
+import Card from "./Card"
+import { Container, Row, Col } from "react-bootstrap"
+
 
 function SearchArea(props) {
-
-    const [photo, setPhoto] = useState("");
-    const [clientId, setClientId] = useState("YYYw7oE7qPoGGH0TZb3J-h7stckt87pGkvD3-019_bk#");
-    const [answer, setAnswer] = useState([]);
-    const [page, setPage] = useState(1);
-
+    const [photo, setPhoto] = useState("")
+    const [clientId, setClientId] = useState(
+        "YYYw7oE7qPoGGH0TZb3J-h7stckt87pGkvD3-019_bk#"
+    )
+    const [answer, setAnswer] = useState([])
+    const [page, setPage] = useState(1)
 
     function handleChange(event) {
-        setPhoto(event.target.value);
+        setPhoto(event.target.value)
     }
 
     const url =
@@ -28,65 +28,88 @@ function SearchArea(props) {
         "&per_page=30&client_id=" +
         clientId +
         "&page=" +
-        page;
+        page
 
     function handleClick() {
         axios.get(url).then((response) => {
-            console.log(response);
-            setAnswer(response.data.results);
-        });
+            console.log(response)
+            setAnswer(response.data.results)
+        })
     }
     const fetchImages = () => {
-        axios.get(url)
+        axios
+            .get(url)
             .then((response) => {
-                setAnswer([...answer, ...response.data.results]);
+                setAnswer([...answer, ...response.data.results])
             })
             .catch((error) => {
-                console.log(error);
-            });
-        setPage(page + 1);
-    };
+                console.log(error)
+            })
+        setPage(page + 1)
+    }
     const handleKeypress = (e) => {
         //it triggers by pressing the enter key
         if (e.charCode === 13) {
-            handleClick();
+            handleClick()
         }
-    };
-
+    }
 
     return (
-        <div className="SearchArea">
-                <InputGroup onChange={handleChange} onKeyPress={handleKeypress} className="col-6">
-                    <FormControl
-                        placeholder="Search Anything!"
-                        aria-label="Search"
-                        aria-describedby="basic-addon2"
-                    />
-                    <Button variant="outline-secondary" id="button-addon2" onClick={handleClick}>
-                        <div className="search-icon">
-                            <SearchIcon />
-                        </div>
-                    </Button>
-
+        <Container fluid>
+            <Row>
+                <Col md={1}></Col>
+                <Col md={10} className="justify-content-center">
+                    <Row className="mt-4">
+                        <Col md={2}></Col>
+                        <InputGroup
+                            onChange={handleChange}
+                            onKeyPress={handleKeypress}
+                            className="col-8"
+                        >
+                            <div className="d-flex align-items-center justify-content-center w-100">
+                                <FormControl
+                                    placeholder="Search Anything!"
+                                    aria-label="Search"
+                                    aria-describedby="basic-addon2"
+                                />
+                                <Button
+                                    variant="primary"
+                                    id="button-addon2"
+                                    onClick={handleClick}
+                                    style={{ width: "150px", height: "42px" }}
+                                >
+                                    <div className="search-icon">
+                                        <SearchIcon />
+                                    </div>
+                                </Button>
+                            </div>
+                        </InputGroup>
+                        <Col md={2}></Col>
+                    </Row>
+                </Col>
+                <Col md={1}></Col>
+            </Row>
+            <Row>
+                <Col md={1}></Col>
+                <Col md={10} className="justify-content-center">
                     <InfiniteScroll
                         dataLength={answer.length}
                         next={fetchImages}
                         hasMore={true}
                     >
-                                <div className="grid flex">
-                                    {answer.map((photo, index) => (
-                                        <div className="box">
-                                        <Card
-                                            img={photo.urls.small}
-                                            key={index} />
-                                            </div>
-                                    ))}
-
+                        <div className="grid flex">
+                            {answer.map((photo, index) => (
+                                <div className="box">
+                                    <Card img={photo.urls.small} key={index} />
                                 </div>
+                            ))}
+                        </div>
                     </InfiniteScroll>
-                </InputGroup>
-            </div>
-    );
+                </Col>
+                <Col md={1}></Col>
+            </Row>
+        </Container>
+    )
 }
 
-export default SearchArea;
+export default SearchArea
