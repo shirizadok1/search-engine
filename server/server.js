3878794657cdfde0d00d89419f090ce886226e8d
Shiri Zadok <shirizadok1@gmail.com>
2022-08-08 12:59:55 +0300

    server file updated

--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
-const path = require("path")
-const express = require("express")
-const app = express()
-const port = process.env.PORT || 3000
-const publicPath = path.join(__dirname, "..", "build")
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+const publicPath = path.join(__dirname, '..', 'public');
 
-app.use(express.static(publicPath))
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(publicPath, "index.html"))
-})
+app.use(express.static(publicPath));
 
 app.listen(port, () => {
-  console.log(Server is up on port ${port}!)
-})
+   console.log(`Server is up on port ${port}!`);
+});
